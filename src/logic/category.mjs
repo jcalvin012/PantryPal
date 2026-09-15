d494490542598900b51a9a2881cf93d46c655b9a
jcalvin012 <jcalvin123123@gmail.com>
2026-09-15 14:44:35 +0800

    fix: prioritize snack category detection

--- src/logic/category.mjs
+++ src/logic/category.mjs
@@ -1,16 +1,16 @@
 export const CATEGORY_OPTIONS = ['Meat', 'Seafood', 'Dairy', 'Fruits', 'Vegetables', 'Grains', 'Canned', 'Condiments', 'Beverages', 'Snacks', 'Other']
 
 const CATEGORY_RULES = [
+  ['Snacks', ['snack', 'chocolate', 'candy', 'popcorn', 'nuts', 'peanuts', 'chips', 'crackers', 'biscuits', 'cookies', 'granola bar', 'protein bar', 'pretzel']],
   ['Meat', ['chicken', 'beef', 'pork', 'lamb', 'goat', 'steak', 'ground meat', 'ground beef', 'ground pork', 'ham', 'bacon', 'sausage', 'hotdog', 'hot dog', 'turkey', 'duck']],
   ['Seafood', ['fish', 'salmon', 'tuna', 'sardine', 'sardines', 'shrimp', 'prawn', 'crab', 'squid', 'mackerel', 'bangus', 'tilapia']],
   ['Dairy', ['milk', 'cheese', 'yogurt', 'yoghurt', 'butter', 'cream', 'margarine']],
   ['Fruits', ['apple', 'banana', 'orange', 'mango', 'grape', 'watermelon', 'papaya', 'pineapple', 'avocado', 'lemon', 'lime', 'pear', 'peach', 'strawberry', 'melon']],
   ['Vegetables', ['carrot', 'potato', 'tomato', 'onion', 'garlic', 'lettuce', 'cabbage', 'spinach', 'broccoli', 'cauliflower', 'eggplant', 'aubergine', 'okra', 'pepper', 'bell pepper', 'cucumber', 'squash', 'corn', 'mushroom', 'beans']],
   ['Grains', ['rice', 'pasta', 'noodle', 'noodles', 'bread', 'flour', 'oat', 'oats', 'cereal', 'quinoa', 'cornmeal']],
-  ['Canned', ['canned', 'can of', 'instant noodles', 'crackers', 'chips', 'biscuits', 'cookies', 'canned goods']],
+  ['Canned', ['canned', 'can of', 'instant noodles', 'canned goods']],
   ['Condiments', ['ketchup', 'mayonnaise', 'mayo', 'mustard', 'soy sauce', 'vinegar', 'sauce', 'salsa', 'hot sauce']],
   ['Beverages', ['water', 'juice', 'coffee', 'tea', 'soda', 'soft drink', 'drink', 'beer']],
-  ['Snacks', ['snack', 'chocolate', 'candy', 'popcorn', 'nuts', 'peanuts', 'chips', 'crackers', 'biscuits', 'cookies']],
 ]
 
 const normalize = (value) => String(value || '').trim().toLowerCase().replace(/\s+/g, ' ')
